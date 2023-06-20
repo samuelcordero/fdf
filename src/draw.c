a40348dd1422879a793f25576516ba3c6aca31e4
Samuel Cordero Rodrigo <sacorder@c1r11s2.42madrid.com>
2023-06-20 16:11:42 +0200

    modified drawsegment

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 /*   By: sacorder <sacorder@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 13:09:41 by sacorder          #+#    #+#             */
-/*   Updated: 2023/06/20 13:25:03 by sacorder         ###   ########.fr       */
+/*   Updated: 2023/06/20 13:39:44 by sacorder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,28 +40,26 @@ void	ft_putimg(void *mlx, void *win, int **img)
 	int endian;
 	char *buffer = mlx_get_data_addr(image, &pixel_bits, &line_bytes, &endian);
 
-	
-
 	for(int y = 0; y < WIN_HEIGHT; ++y)
 		for(int x = 0; x < WIN_WIDTH; ++x)
 		{
 			color = img[y][x];
 			if (pixel_bits != 32)
-  	  			color = mlx_get_color_value(mlx, color);
-		    int pixel = (y * line_bytes) + (x * 4);
+				color = mlx_get_color_value(mlx, color);
+			int pixel = (y * line_bytes) + (x * 4);
 
-    		if (endian == 1)        // Most significant (Alpha) byte first
-    		{
-       			buffer[pixel + 0] = (color >> 24);
-        		buffer[pixel + 1] = (color >> 16) & 0xFF;
-		        buffer[pixel + 2] = (color >> 8) & 0xFF;
-        		buffer[pixel + 3] = (color) & 0xFF;
-		    }
+			if (endian == 1)        // Most significant (Alpha) byte first
+			{
+				buffer[pixel + 0] = (color >> 24);
+				buffer[pixel + 1] = (color >> 16) & 0xFF;
+				buffer[pixel + 2] = (color >> 8) & 0xFF;
+				buffer[pixel + 3] = (color) & 0xFF;
+			}
     		else if (endian == 0)   // Least significant (Blue) byte first
-		    {
+			{
         		buffer[pixel + 0] = (color) & 0xFF;
-		      buffer[pixel + 1] = (color >> 8) & 0xFF;
-		        buffer[pixel + 2] = (color >> 16) & 0xFF;
+				buffer[pixel + 1] = (color >> 8) & 0xFF;
+				buffer[pixel + 2] = (color >> 16) & 0xFF;
         		buffer[pixel + 3] = (color >> 24);
 		    }
 		}
