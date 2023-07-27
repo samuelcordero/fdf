8e858eeca12f94f07bb163b381dd3f9d7b191a97
Samuel Cordero Rodrigo <sacorder@c1r11s2.42madrid.com>
2023-07-27 15:04:21 +0200

    testng brasenham algo...

--- src/draw.c
+++ src/draw.c
@@ -6,25 +6,13 @@
 /*   By: sacorder <sacorder@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 13:09:41 by sacorder          #+#    #+#             */
-/*   Updated: 2023/07/26 00:07:10 by sacorder         ###   ########.fr       */
+/*   Updated: 2023/07/27 13:06:04 by sacorder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/fdf.h"
 
-static int interpolate_color(t_point a, t_point b, float total, float remaining)
-{
-	int		pr;
-	int		pb;
-	int		pg;
-	float	t;
 
-	t = remaining / total;
-    pr = (int)(a.color >> 16 & 0xFF) * t + (int)(b.color >> 16 & 0xFF) * (1 - t);
-    pg = (int)(a.color >> 8 & 0xFF) * t + (int)(b.color >> 8 & 0xFF) * (1 - t);
-    pb = (int)(a.color & 0xFF) * t + (int)(b.color & 0xFF) * (1 - t);
-    return ((pr << 16) | (pg << 8) | pb);
-}
 
 static void	img_pix_put(t_img *img, int x, int y, int color)
 {
@@ -60,28 +48,49 @@ static void	render_black_background(t_img *img)
 	}
 }
 
+static int interpolate_color(t_point a, t_point b, float t)
+{
+	int		pr;
+	int		pb;
+	int		pg;
+
+    pr = (int)(a.color >> 16 & 0xFF) * (1 - t) + (int)(b.color >> 16 & 0xFF) * t;
+    pg = (int)(a.color >> 8 & 0xFF) * (1 - t) + (int)(b.color >> 8 & 0xFF) * t;
+    pb = (int)(a.color & 0xFF) * (1 - t) + (int)(b.color & 0xFF) * t;
+    return ((pr << 16) | (pg << 8) | pb);
+}
+
 static void render_line(t_img *img, t_point a, t_point b)
 {
-	int		color;
-	int		total_pixels;
-	double deltaX = b.proy_x - a.proy_x; // 10
-	double deltaY = b.proy_y - a.proy_y; // 0
-	int pixels = sqrt((deltaX * deltaX) + (deltaY * deltaY));
+    int x0 = (int)a.proy_x;
+    int y0 = (int)a.proy_y;
+    int x1 = (int)b.proy_x;
+    int y1 = (int)b.proy_y;
 
-	deltaX /= pixels; // 1
-	deltaY /= pixels; // 0
-	double pixelX = a.proy_x;
-	double pixelY = a.proy_y;
-	total_pixels = pixels;
-	while (pixels)
-	{
-		color = interpolate_color(a, b, total_pixels, pixels);
-   		img_pix_put(img, (int) pixelX, (int) pixelY, color);
-    	pixelX += deltaX;
-    	pixelY += deltaY;
-    	--pixels;
+    int dx = abs(x1 - x0);
+    int dy = abs(y1 - y0);
+    int sx = (x0 < x1) ? 1 : -1;
+    int sy = (y0 < y1) ? 1 : -1;
+
+    int err = dx - dy;
+	//ft_printf("putting line from %i, %i to %i, %i\n", x0, y0, x1, y1);	
+	//sleep(2);
+    while ((x0 != x1 && y0 != y1) && (x0 - sx != x1 && y0 - sy != y1)) {
+		//ft_printf("putting pixel %i, %i\n", x0, y0);	
+        float t = sqrtf((x0 - a.proy_x) * (x0 - a.proy_x) + (y0 - a.proy_y) * (y0 - a.proy_y)) /
+                  sqrtf((b.proy_x - a.proy_x) * (b.proy_x - a.proy_x) + (b.proy_y - a.proy_y) * (b.proy_y - a.proy_y));
+		int pixel_color = interpolate_color(a, b, t);
+        img_pix_put(img, x0, y0, pixel_color);
+        if (2 * err > -dy) {
+            err -= dy;
+            x0 += sx;
+        }
+        if (2 * err < dx) {
+            err += dx;
+            y0 += sy;
+        }
 	}
-}
+} 
 
 void	render_fdf(t_fdf *fdf)
 {
@@ -113,12 +122,26 @@ int	render(t_fdf *fdf)
 	ft_project_iso(fdf->map, angle);
 	render_fdf(fdf);
     mlx_put_image_to_window(fdf->mlx, fdf->win_ptr, fdf->img.mlx_img, 0, 0);
+	fdf->map->h_tile_size += 0.01;
 	//sleep(5);
 	//printf("Angle: %f\n", angle);
     return (0);
 }
 
-/* static void render_line(t_img *img, t_point a, t_point b)
+/* 
+static int interpolate_color(t_point a, t_point b, float t)
+{
+	int		pr;
+	int		pb;
+	int		pg;
+
+    pr = (int)(a.color >> 16 & 0xFF) * (1 - t) + (int)(b.color >> 16 & 0xFF) * t;
+    pg = (int)(a.color >> 8 & 0xFF) * (1 - t) + (int)(b.color >> 8 & 0xFF) * t;
+    pb = (int)(a.color & 0xFF) * (1 - t) + (int)(b.color & 0xFF) * t;
+    return ((pr << 16) | (pg << 8) | pb);
+}
+
+static void render_line(t_img *img, t_point a, t_point b)
 {
     int x0 = (int)a.x;
     int y0 = (int)a.y;
@@ -146,4 +169,42 @@ int	render(t_fdf *fdf)
             y0 += sy;
         }
     }
-} */
+} 
+
+static int interpolate_color(t_point a, t_point b, float total, float remaining)
+{
+	int		pr;
+	int		pb;
+	int		pg;
+	float	t;
+
+	t = remaining / total;
+    pr = (int)(a.color >> 16 & 0xFF) * t + (int)(b.color >> 16 & 0xFF) * (1 - t);
+    pg = (int)(a.color >> 8 & 0xFF) * t + (int)(b.color >> 8 & 0xFF) * (1 - t);
+    pb = (int)(a.color & 0xFF) * t + (int)(b.color & 0xFF) * (1 - t);
+    return ((pr << 16) | (pg << 8) | pb);
+}
+
+static void render_line(t_img *img, t_point a, t_point b)
+{
+	int		color;
+	int		total_pixels;
+	double deltaX = b.proy_x - a.proy_x; // 10
+	double deltaY = b.proy_y - a.proy_y; // 0
+	int pixels = sqrt((deltaX * deltaX) + (deltaY * deltaY));
+
+	deltaX /= pixels; // 1
+	deltaY /= pixels; // 0
+	double pixelX = a.proy_x;
+	double pixelY = a.proy_y;
+	total_pixels = pixels;
+	while (pixels)
+	{
+		color = interpolate_color(a, b, total_pixels, pixels);
+   		img_pix_put(img, (int) pixelX, (int) pixelY, color);
+    	pixelX += deltaX;
+    	pixelY += deltaY;
+    	--pixels;
+	}
+}
+*/
